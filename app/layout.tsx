f57aa0eaee9e6db60e3a88201403881b6555e556
BITNODES <135651866+opsmanager1@users.noreply.github.com>
2025-02-18 10:20:02 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'Warden AI',
+  title: 'Axone AI',
   description: 'Created by BITNODES',
   generator: 'BITNODES',
 }
